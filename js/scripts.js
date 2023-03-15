f2c136a8a1760f958b690266cda9e5b04b97913c
Nika <pavliushin.nikita@mail.ru>
2023-03-15 18:45:40 +0300

    first

---
+++ js/scripts.js
@@ -0,0 +1,152 @@
+// $(window).on('load', function () {
+//     let $preloader = $('#p_prldr'),
+//         $svg_anm = $preloader.find('.svg_anm');
+//     $svg_anm.delay(1000).fadeOut("slow");
+//     $preloader.delay(1000).fadeOut('medium');
+// });
+// $(document).ready(function () {
+//     $(window).scroll(function () {
+//         if ($(window).scrollTop() > 400) {
+//             $('.logo1').fadeIn(900)
+//         } else {
+//             $('.logo1').fadeOut(700)
+//         }
+//     });
+// });
+// $(document).ready(function () {
+//     $(window).scroll(function () {
+//         if ($(window).scrollTop() > 900) {
+//             $('.sovet').fadeIn(900)
+//         } else {
+//             $('.sovet').fadeOut(700)
+//         }
+//     });
+// });
+
+// //Для открытия картинок
+// var showBtn = document.querySelector('.clibable');
+// showBtn.onclick = function () {
+//     document.querySelector('.hidablestoris').style.display = 'block';
+//     document.querySelector('.hide').style.display = 'block';
+// };
+
+// var showBtn = document.querySelector('.clibable1');
+// showBtn.onclick = function () {
+//     document.querySelector('.hidablestoris').style.display = 'block';
+//     document.querySelector('.hide1').style.display = 'block';
+// };
+
+// var showBtn = document.querySelector('.clibable2');
+// showBtn.onclick = function () {
+//     document.querySelector('.hidablestoris').style.display = 'block';
+//     document.querySelector('.hide2').style.display = 'block';
+// };
+
+// var showBtn = document.querySelector('.clibable3');
+// showBtn.onclick = function () {
+//     document.querySelector('.hidablestoris').style.display = 'block';
+//     document.querySelector('.hide3').style.display = 'block';
+// };
+
+// var showBtn = document.querySelector('.clibable3');
+// showBtn.onclick = function () {
+//     document.querySelector('.hidablestoris').style.display = 'block';
+//     document.querySelector('.hide4').style.display = 'block';
+// };
+
+// var showBtn = document.querySelector('.clibable4');
+// showBtn.onclick = function () {
+//     document.querySelector('.hidablestoris').style.display = 'block';
+//     document.querySelector('.hide5').style.display = 'block';
+// };
+
+// var showBtn = document.querySelector('.clibable5');
+// showBtn.onclick = function () {
+//     document.querySelector('.hidablestoris').style.display = 'block';
+//     document.querySelector('.hide6').style.display = 'block';
+// };
+
+// var showBtn = document.querySelector('.clibable6');
+// showBtn.onclick = function () {
+//     document.querySelector('.hidablestoris').style.display = 'block';
+//     document.querySelector('.hide6').style.display = 'block';
+// }
+// var showBtn = document.querySelector('.clibable7');
+// showBtn.onclick = function () {
+//     document.querySelector('.hidablestoris').style.display = 'block';
+//     document.querySelector('.hide7').style.display = 'block';
+// };
+// var showBtn = document.querySelector('.clibable8');
+// showBtn.onclick = function () {
+//     document.querySelector('.hidablestoris').style.display = 'block';
+//     document.querySelector('.hide8').style.display = 'block';
+// };
+
+// // Для закрытия
+// let closeBtn = document.querySelector('.cross');
+// closeBtn.onclick = function () {
+//     document.querySelector('.hidablestoris').style.display = 'none';
+//     document.querySelector('.hide').style.display = 'none';
+//     document.querySelector('.hide1').style.display = 'none';
+//     document.querySelector('.hide2').style.display = 'none';
+//     document.querySelector('.hide3').style.display = 'none';
+//     document.querySelector('.hide4').style.display = 'none';
+//     document.querySelector('.hide5').style.display = 'none';
+//     document.querySelector('.hide6').style.display = 'none';
+//     document.querySelector('.hide7').style.display = 'none';
+//     document.querySelector('.hide8').style.display = 'none';
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let closeBtn = document.querySelector('.cross');
+// closeBtn.onclick = function () {
+//     let showme = document.querySelector('.hidablestoris');
+//     let showmePic0 = document.querySelector('.hide');
+//     let showmePic1 = document.querySelector('.hide1');
+//     let showmePic2 = document.querySelector('.hide2');
+//     if (showme.style.display !== 'none') {
+//         showme.style.display = 'none';
+//         showmePic.style.display = 'none'
+//     }
+//     if (showmePic0.style.display !== 'none') {
+//         showme.style.display = 'none';
+//         showmePic.style.display = 'none'
+//     }
+//     if (showmePic1.style.display !== 'none') {
+//         showme.style.display = 'none';
+//         showmePic.style.display = 'none'
+//     }
+//     if (showmePic2.style.display !== 'none') {
+//         showme.style.display = 'none';
+//         showmePic.style.display = 'none'
+//     }
+//     else {
+//         showme.style.display = 'block';
+//         showmePic.style.display = 'block';
+//     }
+// };
+
+
+// $(function () {
+
+//     setInterval(function () {
+//         $('.leftBottomImg').toggleClass('.leftBottomImg1');
+//     }, 3000);
+
+// });
